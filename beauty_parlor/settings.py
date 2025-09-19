9e4083443b598c59b3afa201a9c9ca8c371f37e7
jef70987 <japhethmasinde3@gmail.com>
2025-09-20 01:59:15 +0300

    latest setting changes

--- beauty_parlor/settings.py
+++ beauty_parlor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['atrium.onrender.com','localhost']
+ALLOWED_HOSTS = ['atrium-wcno.onrender.com','localhost']
 
 
 # Application definition
