3998b06ce1cd0822bd8a34d8d4d56610d50e6652
jef70987 <japhethmasinde3@gmail.com>
2025-09-20 01:09:23 +0300

    latest changes

--- beauty_parlor/settings.py
+++ beauty_parlor/settings.py
@@ -100,12 +100,7 @@ CORS_EXPOSE_HEADERS = [
     'X-CSRFToken'
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'japhethmasinde3@gmail.com'
-EMAIL_HOST_PASSWORD = 'brzjwmvjvwmdtdgs'
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
